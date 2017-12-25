83687c40889f34cc17f6590675eb53fdeeacd7cf
ankita dangi <ankita.dangi@zingbytes.com>
2017-12-25 14:16:35 +0530

    search with using ajax

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,16 +1,11 @@
 class StocksController < ApplicationController
-def search
-  if params[:stock].present?
-    @stock = Stock.new_from_lookup(params[:stock])
-    if @stock
-      render 'users/my_portfolio'
+  def search
+    if params[:stock].blank? 
+      flash.now[:danger] = "You have entered an empty search string"
     else
-      flash[:danger] = "You have entered an incorrect symbol"
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "You have entered an incorrect symbol" unless @stock
     end
-  else
-    flash[:danger] = "You have entered an empty search string"
-    redirect_to my_portfolio_path
+    render partial: 'users/result'
   end
-end
 end
